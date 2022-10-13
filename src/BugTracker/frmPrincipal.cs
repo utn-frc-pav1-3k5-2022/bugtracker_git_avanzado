e1abcf726342f447770b603010221da8bf4e97ac
Ruben Romero <romeroa.ruben@gmail.com>
2022-10-12 21:44:40 -0300

    Nuevo menu ayuda

--- src/BugTracker/frmPrincipal.cs
+++ src/BugTracker/frmPrincipal.cs
@@ -41,5 +41,10 @@ namespace BugTracker
         private void salirToolStripMenuItem_Click(object sender, EventArgs e)
         {
         }
+
+        private void ayudaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Necesita Ayuda?", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+        }
     }
 }
